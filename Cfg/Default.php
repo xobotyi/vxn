97fbab729bb58a41ebeaaecb280c86e0bb723cff
a.zinoviev <a.zinoviev@gaijin.ru>
2017-10-24 11:05:41 +0300

    - Memcache tags versions buffer;

--- Cfg/Default.php
+++ Cfg/Default.php
@@ -11,15 +11,18 @@
     // DATABASES
     $cfg['Vxn']['storage']['postgres']['host']     = '127.0.0.1';
     $cfg['Vxn']['storage']['postgres']['port']     = 5432;
-    $cfg['Vxn']['storage']['postgres']['user']     = '';
-    $cfg['Vxn']['storage']['postgres']['pass']     = '';
-    $cfg['Vxn']['storage']['postgres']['dbname']   = '';
+    $cfg['Vxn']['storage']['postgres']['user']     = 'test';
+    $cfg['Vxn']['storage']['postgres']['pass']     = 'test';
+    $cfg['Vxn']['storage']['postgres']['dbname']   = 'test';
     $cfg['Vxn']['storage']['postgres']['encoding'] = 'UTF8';
+    $cfg['Vxn']['storage']['postgres']['useCache'] = true;
 
     // CACHE
     $cfg['Vxn']['cache']['memcache']['host']           = '127.0.0.1';
     $cfg['Vxn']['cache']['memcache']['port']           = 11211;
+    $cfg['Vxn']['cache']['memcache']['useTagsBuffer']  = true;
     $cfg['Vxn']['cache']['memcache']['ttl']['default'] = 60;
+    $cfg['Vxn']['cache']['memcache']['ttl']['tags']    = 604800;
     $cfg['Vxn']['cache']['memcache']['ttl']['max']     = 2592000;
 
     return $cfg;
